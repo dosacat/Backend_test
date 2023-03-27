67af3c80307d5f6aca530425c53eba8cf8b3c0b6
dosacat <98118185+dosacat@users.noreply.github.com>
2023-03-27 11:41:35 +0400

    Update users.js

--- models/users.js
+++ models/users.js
@@ -4,12 +4,6 @@ const userSchema = new mongoose.Schema({
   username: String,
   name: String,
   passwordHash: String,
-  blogs: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Blog'
-    }
-  ],
 })
 
 userSchema.set('toJSON', {
